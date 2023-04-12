599c7bd642dbd5316884ce095ea6b7aee3d4a3bb
kyzym <kyzym1@gmail.com>
2023-04-12 10:07:48 +0200

    feat: solve delete-digit Wed, Apr 12, 2023 10:07:48 AM

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given two strings, find the number of common characters between them.
@@ -11,11 +11,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  let commonCount = 0;
+  const s1Array = s1.split("");
+  const s2Array = s2.split("");
+
+  for (const char of s1Array) {
+    const indexInS2 = s2Array.indexOf(char);
+    if (indexInS2 !== -1) {
+      commonCount++;
+      s2Array.splice(indexInS2, 1);
+    }
+  }
+
+  return commonCount;
 }
 
 module.exports = {
-  getCommonCharacterCount
+  getCommonCharacterCount,
 };
