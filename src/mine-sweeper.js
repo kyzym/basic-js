3f43030a460624d6c15bfc35a162ecc9a4766884
kyzym <kyzym1@gmail.com>
2023-04-12 10:52:24 +0200

    feat: solve miner  04/12/2023 10:52:24

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,11 +23,49 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  const result = [];
+
+  // Iterate through rows
+  for (let i = 0; i < matrix.length; i++) {
+    result[i] = [];
+
+    // Iterate through columns
+    for (let j = 0; j < matrix[i].length; j++) {
+      let count = 0;
+
+      // Check neighbors in x-axis
+      for (let x = -1; x <= 1; x++) {
+        // Check neighbors in y-axis
+        for (let y = -1; y <= 1; y++) {
+          // Skip the current cell
+          if (x === 0 && y === 0) continue;
+
+          const newX = i + x;
+          const newY = j + y;
+
+          // Check if coordinates are within matrix bounds
+          if (
+            newX >= 0 &&
+            newX < matrix.length &&
+            newY >= 0 &&
+            newY < matrix[i].length
+          ) {
+            // Increase count if neighbor cell has a mine
+            if (matrix[newX][newY]) count++;
+          }
+        }
+      }
+
+      // Add count to the result matrix at the current position
+      result[i][j] = count;
+    }
+  }
+
+  // Return the resulting matrix
+  return result;
 }
 
 module.exports = {
-  minesweeper
+  minesweeper,
 };
